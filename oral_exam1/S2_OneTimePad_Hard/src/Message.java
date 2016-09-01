d90c60cd2279272b6e1282d496db553853ddd65b
Tanner <tanner-brunscheon@uiowa.edu>
2016-09-01 12:04:36 -0500

    Did more work on OTP. Almost done with medium.

--- oral_exam1/S2_OneTimePad_Hard/src/Message.java
+++ oral_exam1/S2_OneTimePad_Hard/src/Message.java
@@ -2,7 +2,7 @@
 /**
  * Created by Tanner on 8/31/2016.
  */
-import javax.lang.model.util.ElementScanner6;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,49 +10,71 @@ public class Message {
     Scanner input = new Scanner(System.in);
     private String messageStr;
     private char[] messageChar;
+    private char[] encryptedChar;
     private Random rando = new Random();
-    private int key;
+    private int[] key;
     private int tempKey;
+    private String strKey;
 
     public Message() {
         System.out.printf("Please Enter Your Message: %n%n");
         messageStr = input.nextLine();
         messageStr = messageStr.toUpperCase();
+        key = new int[messageStr.length()];
     }
-
     public void encryptMessage(){
         messageChar = messageStr.toCharArray();
-        key = rando.nextInt(36);
-        for (int i = 0; i<messageStr.length(); i++)
-        {
+        encryptedChar = messageChar;
+
+        for (int i = 0; i<messageStr.length(); i++) {
+            key[i] = rando.nextInt(36);
             for (int j = 0; j<36; j++){
-                if(messageChar[i]==OTPMenu.alphabet[j])
-                {
-                    if(j+key>36){
-                        tempKey = (j+key)%36;
-                        messageChar[i] = OTPMenu.alphabet[tempKey];
+                if(messageChar[i]==OTPMenu.alphabet[j]) {
+                    if(j+key[i]>35){
+                        tempKey = (j+key[i])%35;
+                        encryptedChar[i] = OTPMenu.alphabet[tempKey];
                         j=36;
                     }
                     else{
-                        messageChar[i] = OTPMenu.alphabet[j+key];
+                        encryptedChar[i] = OTPMenu.alphabet[j+key[i]];
                         j=36;
                     }
                 }
             }
         }
     }
-
-    public int getKey() {
-        return key;
+    public void decryptMessage(){
+        for (int i = 0; i<messageStr.length(); i++) {
+            for (int j = 0; j<36; j++){
+                if(encryptedChar[i]==OTPMenu.alphabet[j]) {
+                    if(j-key[i]<0){
+                       tempKey = (j-key[i])%35*-1;
+                       encryptedChar[i] = OTPMenu.alphabet[tempKey];
+                       j=36;
+                    }
+                    else{
+                       encryptedChar[i] = OTPMenu.alphabet[j-key[i]];
+                       j=36;
+                   }
+               }
+           }
+       }
     }
-
     public String getUnencryptedMessage() {
         return messageStr;
     }
-
-    public String getEncrypedMessage() {
-        // Got from https://stackoverflow.com/questions/7655127/how-to-convert-a-char-array-back-to-a-string
-        String encrypted = new String(messageChar);
-        return encrypted;
+    public String getKey(){
+         strKey = Arrays.toString(key);
+        return strKey;
     }
+    public String getEncryptedMessage() {
+            // Got from https://stackoverflow.com/questions/7655127/how-to-convert-a-char-array-back-to-a-string
+            String encrypted = new String(encryptedChar);
+            return encrypted;
+    }
+    public String getDecryptedMessage(){
+        String decrypted = new String(encryptedChar);
+        return decrypted;
+    }
+
 }
--- oral_exam1/S2_OneTimePad_Hard/src/OTPMenu.java
+++ oral_exam1/S2_OneTimePad_Hard/src/OTPMenu.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class OTPMenu {
     final static char  alphabet [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
     private byte menuChoice = 1;
+    private int key;
     Scanner input = new Scanner(System.in);
+    private Message userMessage = new Message();
 
     public void runMenu(){
-        while(menuChoice>0){
-            System.out.printf("Please Choose From the Following Options: %n1. Input Message%n2. Encrypt Message%n3. Decrypt Message %n");
+        while(menuChoice>0 && menuChoice<3){
+            System.out.printf("Please Choose From the Following Options: %n1. Encrypt %n2. Decrypt %nInsert any other number to quit: ");
             menuChoice =input.nextByte();
             switch (menuChoice){
                 case 1:{
-                    Message userMessage = new Message();
+                    System.out.printf("Please enter your key value: ");
                     userMessage.encryptMessage();
                     System.out.printf("Input Message:  " + userMessage.getUnencryptedMessage());
-                    System.out.printf("%nEncrypted Message:  " + userMessage.getEncrypedMessage());
-                    System.out.printf("%nKey:  " + userMessage.getKey() +"%n%n");
+                    System.out.printf("%nEncrypted Message:  " + userMessage.getEncryptedMessage() + "T%n");
+                    System.out.printf("Key:  " + userMessage.getKey() + "%n%n");
+                    break;
                 }
+                case 2:{
+                    System.out.printf("Please enter your key value: ");
+                    System.out.printf("%nEncrypted Message:  " + userMessage.getEncryptedMessage());
+                    System.out.printf("%nKey:  " + userMessage.getKey() + "%n%n");
+                    userMessage.decryptMessage();
+                    System.out.printf("%nDecrypted Message:  " + userMessage.getDecryptedMessage()+ "%n");
+                    break;
+                }
+
             }
         }
     }
