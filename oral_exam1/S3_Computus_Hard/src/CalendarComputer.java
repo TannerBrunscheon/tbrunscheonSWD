c25d67c5c67515e63511528ee7df09e9767c467a
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2016-09-09 11:41:05 -0500

    Added Months array

--- oral_exam1/S3_Computus_Hard/src/CalendarComputer.java
+++ oral_exam1/S3_Computus_Hard/src/CalendarComputer.java
@@ -4,6 +4,7 @@ import com.sun.corba.se.impl.orb.ParserTable;
  * Created by Tanner on 8/31/2016.
  */
 public class CalendarComputer {
+    final static String MONTH [] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
     public static void main(String[] args) {
 
         Easter e = new Easter(2008);
