10506c3788fb7cb38f0b042963d9f9ad8f6eb251
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2016-09-09 11:50:58 -0500

    Set up everything for Easter. No computing yet.

--- oral_exam1/S3_Computus_Hard/src/Easter.java
+++ oral_exam1/S3_Computus_Hard/src/Easter.java
@@ -1,10 +1,27 @@
+import java.time.Month;
+
 /**
  * Created by Tanner on 8/31/2016.
  */
 public class Easter {
     private int year;
+    private int[] dayMonth;
+    final static String MONTH[] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 
 
+    public Easter(int year) {
+        this.year = year;
+    }
 
+    public void computeEaster(){
 
+    }
+
+    public String getMonth() {
+        return MONTH[dayMonth[1]];
+    }
+
+    public int getDay(){
+        return dayMonth[0];
+    }
 }
