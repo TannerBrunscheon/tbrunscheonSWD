80ed10281f96d8c12d9fe313963c7a460951bb8a
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2016-09-14 11:57:20 -0500

    Panic Commit

---
+++ oral_exam1/S3_Computus_Hard/test/UserComputerTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tbrunscheon on 9/14/16.
+ */
+public class UserComputerTest {
+    @org.junit.Test
+    public void testUserComputer() throws Exception {
+        new Us
+
+    }
+}
