dfef68a1631e85c4e6eb28832b54337a90344c14
Tanner <tanner-brunscheon@uiowa.edu>
2016-09-20 22:14:30 -0500

    Cant figure out Enum

--- oral_exam1/S15_ArabicToRomanGUI/src/RomanConverter.java
+++ oral_exam1/S15_ArabicToRomanGUI/src/RomanConverter.java
@@ -4,7 +4,9 @@ import sun.plugin2.message.Conversation;
  * Created by Tanner on 9/19/2016.
  */
 public class RomanConverter {
-    private final char[] NUMBERS ={'I','V','X','L','C','D','M'};
+    enum Numerals{
+
+    }
     private String number;
 
     public RomanConverter(String number) {
@@ -14,7 +16,7 @@ public class RomanConverter {
     private Conversion(){
         for (int i =0;i<number.length();i++) {
             switch (number.charAt(i)){
-                case I:
+                case
 
 
 
