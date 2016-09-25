5b34ce59dfa443ab632bd5ff5d7eda5f9319dff9
Tanner <tanner-brunscheon@uiowa.edu>
2016-09-25 17:21:03 -0500

    Color Chooser started

--- oral_exam1/22-13_ColorChooser/src/ColorTest.java
+++ oral_exam1/22-13_ColorChooser/src/ColorTest.java
@@ -3,12 +3,13 @@ import javax.swing.*;
 /**
  * Created by Tanner on 9/25/2016.
  */
-public class ColorGUI {
+public class ColorTest {
     public static void main(String[] args) {
         MyColorChooser myColorChooser = new MyColorChooser();
-        myColorChooser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myColorChooser.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         myColorChooser.setSize(300,300);
         myColorChooser.setVisible(true);
 
     }
 }
+
