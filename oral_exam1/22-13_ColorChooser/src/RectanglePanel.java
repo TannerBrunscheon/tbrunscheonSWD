caae0dc06ac4cfb4e51fa6a789e2951a3b04c6bc
Tanner <tanner-brunscheon@uiowa.edu>
2016-10-03 17:16:41 -0500

    Comments

--- oral_exam1/22-13_ColorChooser/src/RectanglePanel.java
+++ oral_exam1/22-13_ColorChooser/src/RectanglePanel.java
@@ -6,8 +6,15 @@ import java.awt.*;
  * Created by Tanner on 9/25/2016.
  */
 public class RectanglePanel extends JPanel {
+    /**
+     * Stored RGB value used to color the rectangle
+     */
     private Color color = new Color(0,0,0);
 
+    /**
+     * Overwrites paint component adding setBackground to color the component.Is only called during repaint.
+     * @param g Graphics created by this class.
+     */
     @Override
     public void paintComponent(Graphics g)
     {
@@ -15,6 +22,12 @@ public class RectanglePanel extends JPanel {
         setBackground(color);
     }
 
+    /**
+     * Changes the color in this object and repaints the panel.
+     * @param red Red part of the RGB in color.
+     * @param green Green part of the RGB in color.
+     * @param blue Blue part of in RGB in color.
+     */
     public void setColor(int red,int green,int blue)
     {
         color = new Color(red,green,blue);
